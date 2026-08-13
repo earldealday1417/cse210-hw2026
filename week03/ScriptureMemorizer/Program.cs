54ab873a5e36457071afcc4c7ab51d88feb6ca6a
earldealday1417 <earldealday1417@gmail.com>
2026-08-14 03:55:44 +0800

    Week03 ScriptureMemorizer - CSE210hw2026

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,3 +1,9 @@
+// Week03 ScriptureMemorizer - CSE210hw2026
+// This program allows the user to memorize a scripture by displaying it and progressively hiding words.
+// The user can press Enter to hide random words or type 'quit' to exit the program. The program continues until all words are hidden or the user chooses to quit.
+// The program uses a Reference class to represent the scripture reference and a Scripture class to manage the scripture text and its display state.
+
+
 using System;
 
 class Program
